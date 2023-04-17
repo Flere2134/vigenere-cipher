a4de53251d989e13d6609a167331b32bf4e7f6af
Flere21 <aaronhans2134@gmail.com>
2023-04-17 22:26:24 +0800

    added the code for converting the message into numbers with use of keyword and reverting to text

--- vigenerecipher.py
+++ vigenerecipher.py
@@ -1,21 +1,21 @@
 #vigenere cipher function
-#def vigenere_cipher(plaintext, keyword):
+def vigenere_cipher(plaintext, keyword):
 #convert plaintext to uppercase and no spaces
-    #plaintext = plaintext.upper().replace(' ', '')
+    plaintext = plaintext.upper().replace(' ', '')
 #convert keyword to uppercase
-    #keyword = keyword.upper()
+    keyword = keyword.upper()
 ##convert keyword to number
+    keyword_convert_to_num = [ord(keyword[i]) - 65 for i in range(len(keyword))]
 #variable for ciphered text
+    ciphered_text = ''
 #convert plaintext to number, get modulo value of whole message using keyword
+    for i in range(len(plaintext)):
+        modulo_value = (ord(plaintext[i]) - 65 + keyword_convert_to_num[i % len(keyword)]) % 26
+        ciphered_text += chr(modulo_value + 65)
 #return value of ciphered text
-
+    return ciphered_text
 #ask user for input of plaintext and keyword
 plaintext = input('Write the message you wanted to cipher using Vigenere Cipher: ')
 keyword = input('Enter the keyword: ')
 #encryption of plaintext
 #print output
-
-plaintext = plaintext.upper().replace(' ', '')
-keyword = keyword.upper()
-
-print(plaintext, keyword)
