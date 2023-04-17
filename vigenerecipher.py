a8ac5f5e2e4130f1003b36b9101626052749da7d
Flere21 <aaronhans2134@gmail.com>
2023-04-17 22:13:19 +0800

    pseudocode

---
+++ vigenerecipher.py
@@ -0,0 +1,9 @@
+#vigenere cipher function
+#convert plaintext to uppercase and no spaces
+#convert keyword to uppercase
+#variable for ciphered text
+#convert plaintext to number, get modulo value of whole message using keyword
+#return value of ciphered text
+#ask user for input of plaintext and keyword
+#encryption of plaintext
+#print output
